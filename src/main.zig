bad78d0c045d729b79bbae838c6228b0f62e7e22
Tony Bradley <antbradley91@gmail.com>
2024-10-30 23:37:13 -0400

    fmt

--- src/main.zig
+++ src/main.zig
@@ -78,7 +78,6 @@ pub fn main() anyerror!void {
 
         system.playerControlsSystems(keyboard, scene, world);
         system.runGravitySystem(delta, world);
-
         try system.runCollisionSystem(delta, &scene, world);
         system.runEntityCollisionSystem(delta, scene, world);
         system.runMovementSystem(delta, world);
